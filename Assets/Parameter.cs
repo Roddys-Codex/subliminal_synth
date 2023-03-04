929c5629fad41b6e38e14b7452d2a2b916cd63cc
Daniel Roddy <daniel.kevin.roddy@gmail.com>
2023-03-04 20:25:20 +0000

    Basic parameter control implemented

--- Assets/Parameter.cs
+++ Assets/Parameter.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using AudioHelm;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class Parameter : MonoBehaviour
 {
-    public HelmController controller;
     public Param param;
+    public TMP_Dropdown parameter;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +20,21 @@ public class Parameter : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void updateParam()
+    {
+        List<string> option = new List<string>();
+        string[] allParams = Enum.GetNames(typeof(Param));
+        //controller.UpdateAllParameters();
+        int count = Enum.GetValues(typeof(CommonParam)).Length;
+        //List<string> enu
+        for (int i = 0; i < 16; i++)
+        {
+            option.Add(allParams[i]);
+        }
+
+        parameter.ClearOptions();
+        parameter.AddOptions(option);
     }
 }
---
+++ Assets/ParameterSlider.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AudioHelm;
+using AudioSynthesis.Synthesis;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParameterSlider : MonoBehaviour
+{
+    public TMP_Dropdown parameter;
+    public Slider paramSlider;
+    public HelmController helmController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setParameter()
+    {
+        //helmController.synthParameters[parameter.options[parameter.].text];
+        string value = parameter.options[parameter.value].text;
+        Debug.Log("VALUE = " + value);
+        //Param param;
+        //Param.TryParse(value, out param);
+        Enum.TryParse(value, out Param paramEnum);
+
+        HelmParameter helmParameter = new HelmParameter();
+        helmParameter.parameter = paramEnum;
+        Debug.Log("PARAM = " + paramEnum);
+        //Debug.Log("FOUND PARAMETER = "+helmController.synthParameters.Find(p => p.parameter.Equals(param)).ToString());
+        //if(!helmController.synthParameters.Contains(helmParameter))
+        //{
+            //helmController.AddParameter(paramEnum);
+        //}
+        
+        helmController.SetParameterValue(paramEnum, paramSlider.value);
+        //helmController.synthParameters.Find(p => p.parameter.Equals(paramEnum)).paramValue = paramSlider.value;
+        //helmController.UpdateAllParameters();
+    }
+}
