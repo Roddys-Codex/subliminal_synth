190028a3f6aff9e9bffa2d5d98027f029813fd26
Daniel Roddy <daniel.kevin.roddy@gmail.com>
2023-03-04 15:20:11 +0000

    Basic preset menu with folder structure up and running. The UI still needs finished of this menu but the back end is functioning

--- Assets/parameterScript.cs
+++ Assets/parameterScript.cs
@@ -7,39 +7,23 @@ using static TMPro.TMP_Dropdown;
 using AudioHelm;
 using MoreLinq;
 using static System.Net.WebRequestMethods;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEngine.Windows;
+using NRKernal;
 
 public class parameterScript : MonoBehaviour
 {
 
-    string filepath = "Assets/AudioHelm/Presets/Arp/";
-    public TMP_Dropdown arpDropDown;
+    string filepath;
+    public TMP_Dropdown folderDropdown;
+    public TMP_Dropdown presetDowndown;
     public HelmController helmController;
     // Start is called before the first frame update
     void Start()
     {
-        string[] files = Directory.GetFiles(filepath, "*.helm", SearchOption.AllDirectories);
-        List<string> option = new List<string>();
-        //files.ForEach(file =>
-        //    file.Remove(filepath.Length));
-        //    //option.Add(file);
-        //);
-        // files.ForEach(file =>
-        //    //file.Remove(filepath.Length));
-        //    file.Substring(filepath.Length)
-        //);
-        files.ForEach(file =>
-            //file.Remove(filepath.Length));
-            option.Add(file.Substring(filepath.Length))
-            );
-        foreach (string op in option)
-        {
-            Debug.Log("OPTION = " + op);
-        }
-        //Debug.Log("FILES = " + files[0]);
-        arpDropDown.ClearOptions();
-        //OptionData dropdownOptionData = new OptionData();
-        //dropdownOptionData.text += files;
-        arpDropDown.AddOptions(option);
+        
+        
 
 
     }
@@ -53,8 +37,47 @@ public class parameterScript : MonoBehaviour
     public void loadPatch()
     {
         HelmPatch patch = new HelmPatch();
-        patch.LoadPatchData(filepath+arpDropDown.options[arpDropDown.value].text);
+        patch.LoadPatchData(filepath+ presetDowndown.options[presetDowndown.value].text +".helm");
         helmController.LoadPatch(patch);
+    }
+
+    public void updateFolder()
+    {
+        this.gameObject.SetActive(true);
+        filepath = "Assets/AudioHelm/Presets/" + folderDropdown.options[folderDropdown.value].text +"/";
+
+        string[] files = System.IO.Directory.GetFiles(filepath, "*.helm", SearchOption.AllDirectories);
+        List<string> option = new List<string>();
+
+        //string text = "ImageDimension=655x0;ThumbnailDimension=0x0";
+        string pattern = @"/(?:[^/]*\/){3}([^.]*)\.";
+
+        files.ForEach(file =>
+        {
+            Match match = Regex.Match(file, pattern);
+
+            if (match.Success)
+            {
+                string result = match.Groups[1].Value;
+                option.Add(match.Groups[1].Value);
+            }
+            
+            Debug.Log("File = " + file);
+            Debug.Log("Matcher Value = " + match.Groups["fileName"].Value.ToString());
+            //file.Remove(filepath.Length));
+            //option.Add(Regex.Split(file, ""));
+        });
+            //option.Add(file.Substring(filepath.Length))
+            //);
+        foreach (string op in option)
+        {
+            Debug.Log("OPTION = " + op);
+        }
+        //Debug.Log("FILES = " + files[0]);
+        presetDowndown.ClearOptions();
+        //OptionData dropdownOptionData = new OptionData();
+        //dropdownOptionData.text += files;
+        presetDowndown.AddOptions(option);
     }
 
 
