e4d0bfc713f0b9f8cc6f4bf0e711b4fdc576ec88
Daniel Roddy <daniel.kevin.roddy@gmail.com>
2023-04-09 15:05:10 +0100

    refactor underway

---
+++ Assets/subliminal_synth_scripts/ColourChange.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColourChange : MonoBehaviour
+{
+    public Color activeColor;
+    public Toggle m_Toggle;
+    public GameObject objectToActivate;
+    public List<GameObject> objectsToActivate;
+    public bool multiObject = false;
+    
+    void Start()
+    {
+        if(multiObject)
+        {
+            m_Toggle = GetComponent<Toggle>();
+
+            m_Toggle.onValueChanged.AddListener(delegate {
+                switchColourAndSetActive();
+            });
+        }
+    }
+
+    public void switchColourAndSetActive()
+    {
+
+        if(GetComponent<MeshRenderer>().material.color!=activeColor)
+        {
+            GetComponent<MeshRenderer>().material.color = activeColor;
+            objectToActivate.SetActive(true);
+
+        } else
+        {
+            GetComponent<MeshRenderer>().material.color = Color.white;
+            objectToActivate.SetActive(false);
+        }
+        
+    }
+
+    public void switchOnlyColour()
+    {
+
+        if (GetComponent<MeshRenderer>().material.color != activeColor)
+        {
+            GetComponent<MeshRenderer>().material.color = activeColor;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+
+    }
+
+    public void setObjectsActive()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        for (int i = 0; i < objectsToActivate.Count; i++)
+        {
+            if (meshRenderer.material.color != activeColor)
+            {
+                objectsToActivate[i].SetActive(true);
+            }
+            else
+            {
+                objectsToActivate[i].SetActive(false);
+            }
+        }
+        switchOnlyColour();
+    }
+
+    public void setObjectsActiveByToggle()
+    {
+        objectsToActivate.ForEach(item =>
+        {
+            if (GetComponent<Toggle>().isOn)
+            {
+                item.SetActive(true);
+            }
+            else
+            {
+                item.SetActive(false);
+            }
+        });
+        switchOnlyColour();
+    }
+}
